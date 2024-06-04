d3243723905f7656b879e9cbeb324fe2e6c2cd2e
Guillaume Doussin <guillaume.doussin@amadeus.com>
2024-06-04 12:26:10 +0200

    Add print

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ def hello():
     return render_template('index.html')
 
 if __name__ == "__main__":
+    print "Running app"
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
